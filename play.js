d40608f246cf59ee1d8ba8a5e6efda6de86fc67d
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-12 18:58:56 +0000

    adding input.js

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 // const net = require('net');
 const connect = require('./client');
+const connect = require('./input');
 
 /**
  * Establishes connection with the game server
@@ -21,26 +22,6 @@ connect();
  * Specifically, so that we can handle user input via stdin
  */
 
-const handleUserInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
 
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput()
-  return stdin;
-}
 
 setupInput();
